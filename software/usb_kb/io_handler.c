b458f6cee54ed1bb2d7dd5558e579a121af7d0c1
trooper306 <61365500+trooper306@users.noreply.github.com>
2020-03-22 21:40:28 -0500

    push some code

--- software/usb_kb/io_handler.c
+++ software/usb_kb/io_handler.c
@@ -19,21 +19,21 @@ void IO_init(void)
 
 void IO_write(alt_u8 Address, alt_u16 Data)
 {
-//*************************************************************************//
-//									TASK								   //
-//*************************************************************************//
-//							Write this function							   //
-//*************************************************************************//
+	*otg_hpi_address=alt_u8 Address;	//set address
+	*otg_hpi_w=0;						//write enable
+	*otg_hpi_data=alt_u16 Data;			// load data
+	*otg_hpi_w=1;
+
 }
 
 alt_u16 IO_read(alt_u8 Address)
 {
 	alt_u16 temp;
-//*************************************************************************//
-//									TASK								   //
-//*************************************************************************//
-//							Write this function							   //
-//*************************************************************************//
-	//printf("%x\n",temp);
+
+	*otg_hpi_address=alt_u8 Address;	//set address to PIO block
+	*otg_hpi_r=0;						//enable read
+	temp=*otg_hpi_data;					//load value into temp
+	*otg_hpi_r=1;						//disable read
+//	printf("%x\n",temp);
 	return temp;
 }
